c1a170347c785ad06470af21c483347cf576691e
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2022-06-08 01:05:58 +0700

    init project

---
+++ RoomAh/app/src/main/java/com/bangkit/roomah/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.roomah.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.roomah.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
