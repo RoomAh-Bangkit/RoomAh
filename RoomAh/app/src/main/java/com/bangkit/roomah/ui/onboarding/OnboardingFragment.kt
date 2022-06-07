939839ea516eb4672cd01e6bd12c1912fe76ec56
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2022-06-08 01:19:23 +0700

    init viewpager & nav graph

---
+++ RoomAh/app/src/main/java/com/bangkit/roomah/ui/onboarding/OnboardingFragment.kt
@@ -0,0 +1,28 @@
+package com.bangkit.roomah.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bangkit.roomah.R
+import com.bangkit.roomah.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
---
+++ RoomAh/app/src/main/java/com/bangkit/roomah/ui/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,22 @@
+package com.bangkit.roomah.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bangkit.roomah.R
+import com.bangkit.roomah.databinding.FragmentViewPagerBinding
+
+class ViewPagerFragment : Fragment() {
+
+    private var _binding: FragmentViewPagerBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_view_pager, container, false)
+    }
+}
