db0ab560e2863ef545de84123c37bd5289c6c0ba
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2022-06-08 22:25:26 +0700

    add sharedpref onboarding

---
+++ RoomAh/app/src/main/java/com/bangkit/roomah/ui/camera/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.roomah.ui.camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.roomah.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
