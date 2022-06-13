eb3bc193e5cd5e2c86c3049a80bfb1174c8177fa
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2022-06-13 10:15:24 +0700

    add rotation for handle case on most device

--- RoomAh/app/src/main/java/com/bangkit/roomah/utils/FileHandler.kt
+++ RoomAh/app/src/main/java/com/bangkit/roomah/utils/FileHandler.kt
@@ -69,8 +69,10 @@ object FileHandler {
         val matrix = Matrix()
 
         return if (isBackCamera) {
+            matrix.postRotate(90f)
             Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
         } else {
+            matrix.postRotate(-90f)
             matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
             Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
         }
